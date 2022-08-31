2422935c0a2bfa6d13def3953e9919afdeaa8492
Sutton <suttonpage2000@gmail.com>
2022-08-31 10:24:51 -0400

    created default class to create chat room

---
+++ src/xml/Room.java
@@ -0,0 +1,83 @@
+package xml;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jdom2.CDATA;
+import org.jdom2.Comment;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+
+public class Room {
+
+    private String userName;
+    private String roomName;
+    private String roomStyle;
+    private Document roomXml;
+
+
+    public Room(String roomName,String userName,String roomStyle){
+
+        this.roomName = roomName;
+        this.userName = userName;
+        this.roomStyle = roomStyle;
+    }
+
+    public void createRoom() throws IOException{
+
+        // Creating xml chatroom document
+        this.roomXml = new Document();
+
+        this.roomXml.setRootElement(new Element("ROOM"));
+
+        // Creating the basic room information elements
+        Element roomName = new Element("NAME");
+        roomName.setText(this.roomName);
+
+        Element roomStyle = new Element("STYLE");
+        roomStyle.setText(this.roomStyle);
+
+        // Creating user data holder
+        Element userNames = new Element("USERS");
+        Element user = new Element(this.userName);
+        userNames.addContent(user);
+
+        // Creating messages holder
+
+        Element mess = new Element("MESSAGES");
+
+        Element head = new Element("HEAD");
+
+
+        // Adding all elements to root element
+
+        Element[] items = {roomName,userNames,mess,head};
+
+        for(int i = 0; i < items.length; i++){
+
+            this.roomXml.getRootElement().addContent(items[i]);
+        }
+
+        
+        XMLOutputter xmlOutputter = new XMLOutputter();
+
+        // pretty print
+        xmlOutputter.setFormat(Format.getPrettyFormat());
+        xmlOutputter.output(this.roomXml,System.out);
+
+        
+    }
+
+
+    
+
+
+
+
+
+    
+}
